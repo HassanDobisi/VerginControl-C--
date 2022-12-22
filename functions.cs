d8e5ef80e5a2c67c7354184b85910825e35d5f2b
HassanDobisi <h_dubaisi@hotmail.com>
2022-12-22 22:16:19 +0300

    update

--- functions.cs
+++ functions.cs
@@ -1,4 +1,13 @@
 namespace VERGINCONTROL 
 {
-
+    public class functions
+    {
+        public string TakeGradeByPercentage(percentage)
+        {
+            if (percentage>=90 && percentage<=100)
+            return "A+";
+            else if (percentage>=80 && percentage <=90)
+            return "B+";
+        }
+    }
 }
