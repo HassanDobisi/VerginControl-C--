2c41397d154c3728d983435f37000b8ab6ee3205
HassanDobisi <h_dubaisi@hotmail.com>
2022-12-25 09:01:40 +0300

    adding the the assert to my test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,6 +8,16 @@ namespace StringLibraryTest
         [TestMethod]
         public void TestMethod1()
         {
+                    public void TakeGradeByPercentage_EqualTest() 
         }
+            //Asign
+            var percentage = 99;
+            
+            // Act
+            var grade = _gradeCalculator.GetGradeByPercentage(percentage);
+
+            // Assert
+            Assert.That(grade, Is.EqualTo("A"));
+
     }
 }
