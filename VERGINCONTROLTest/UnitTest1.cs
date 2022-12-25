cca349d5a4850aa00c666620502a3aba1aaef817
HassanDobisi <h_dubaisi@hotmail.com>
2022-12-25 09:25:42 +0300

    delete the old test & make new unitTest

---
+++ VERGINCONTROLTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityLibraries;
+namespace VERGINCONTROLTest;
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TakeGradeByPercentage()
+    {
+        
+    }
+}
